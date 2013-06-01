8aba1df339d98b8a4ec6f11d852b947a93bb51ee
liyanfeng <muxsdt@gmail.com>
2013-06-02 00:49:44 +0800

    adjust code

--- src/iloveh/core.clj
+++ src/iloveh/core.clj
@@ -9,13 +9,16 @@
      org.httpkit.server
      iloveh.msgtmpl))
 
+;;;微信token
 (def TOKEN "lyfpcy")
 
-(def HELP "输入格式: \"@A 喜欢 @B 这是我想对你说的话\"，其中A为你自己的微信号，B为对方的微信号，后面一句是你想对ta说的话，输入\"c\"查询是否有人喜欢你，输入\"h\"请求帮助")
+;;;返回信息
+(def HELP "输入: @微信号 xxx，其中xxx为你对ta的表白，输入\"c\"查询是否有人喜欢你，输入\"h\"请求帮助")
+(def REGISTER-HELP "请先注册信息, 输入你的微信号与电子邮件地址,即可完成注册，注意用空格隔开。\n例如: xiaoming123 xm1990@gmail.com\n")
+(def DEFAULT-RETURN "请耐心等待，如果ta也喜欢你，我们会邮件通知你们俩的")
+(def WELCOME-MSG (str "谢谢关注我们的帐号，" REGISTER-HELP HELP))
 
-(defn infos [req]
-  (mc/find-maps "loves"))
-
+;;;微信url授权验证
 (defn auth [req]
   (let [echostr (-> req :params :echostr)
         signature (-> req :params :signature)
@@ -28,14 +31,77 @@
       echostr
       (println "auth failed!"))))
 
-(defn resplove [info]
-  (apply str (map #(format "你喜欢的人 %s 也喜欢你哦, ta对你说: %s \n" (:a %) (:loveword %)) info)))
+;;;发送邮件通知双方互相喜欢
+(defn send-match-mail [a b asaid bsaid]
+  (println "like each other " a b asaid bsaid)
+  (str "恭喜你们俩，你们互相喜欢\n" (format-sweetwords asaid) "\n" (format-sweetwords bsaid)))
 
-(defn love [fromid a b loveword]
-  (println fromid a b loveword)
+;;;格式化a对b说的话
+(defn format-sweetwords [words]
+  (apply str (map #(format "%s 喜欢 %s, %s 对 %s 说: %s \n" (:a %) (:b %) (:a %) (:b %) (:sweetwords %)) words)))
+
+;;;注册用户
+(defn register [openid name email]
+  (mc/insert "users" {:openid openid :name name :emal emal}))
+
+;;;查看是否相互喜欢
+(defn like-each-other? [a b]
+  (let [[alikeb asaid] (like? a b)
+        [blikea bsaid] (like? b a)]
+    (if (and alikeb blikea)
+      [true asaid bsaid]
+      [false "" ""])))
+
+;;;判断a是否喜欢b
+(defn like? [a b]
+  (let [ret (mc/find-maps "messages" {:a a :b b})]
+    (if (empty? ret)
+      [false ""]
+      [true ret])))
+
+(defn get-register-info [openid]
+  (mc/find-one-as-map "users" {:openid openid}))
+
+(defn registered? [openid]
+  (nil? (get-register-info openid)))
+
+;;;你喜欢的人和喜欢你的人是否匹配
+(defn match-message [be-liked like]
+  (println be-liked like)
+  (if (empty? be-liked)
+    "请耐心等待"
+    (if (empty? like)
+      "有人喜欢你哦，快@你喜欢的人，看看你们是否相互喜欢"
+      "")))
+
+(defn check-liked [openid]
+  (let [info (get-register-info openid)]
+    (if (nil? info)
+      REGISTER-HELP
+		  (let [be-liked (mc/find-maps "messages" {:b (:name info)})
+          like (mc/find-maps "messages" {:a (:name info)})]
+		    (match-message be-liked like)))))
+
+;;;处理用户的表白信息
+(defn speak-to-ta [your-openid ta sweetwords]
+  (let [ret (get-register-info your-openid)]
+    (if (nil? ret)
+      REGISTER-HELP
+      (let [you (:name ret)]
+        (if (mg/ok? (mc/insert "messages" {:a you :b ta :sweetwords sweetwords}))
+          (let [[match tasaid yousaid] (like-eachother? ta you)]
+            (if match
+              (send-match-mail ta you tasaid yousaid)
+              DEFAULT-RETURN)))))
+          "数据库插入错误")
+        
+        
+;;;///////////////////////////////////////////
+(defn love [fromid a b sweetwords]
+  (println fromid a b sweetwords)
   (let [ret (mc/find-maps "loves" {:fromid fromid})]
     (if (or (empty? ret) (= (:a (first ret)) a))
-		  (do (mc/insert "loves" {:fromid fromid :a a :b b :loveword loveword})
+		  (do (mc/insert "loves" {:fromid fromid :a a :b b :sweetwords sweetwords})
       (let [ret2 (mc/find-maps "loves" {:a b :b a})]
 		    (if (empty? ret2)
 		      (format "请耐心等待，你喜欢的人 %s 或许也正暗恋着你" b)
@@ -52,10 +118,13 @@
         (if (empty? ret2)
           (format "请耐心等待，你喜欢的人或许还没不认识我哦，你可以跟ta介绍下我呀^_^")
           (resplove ret2))))))
+;;;/////////////////////////////////////////
 
+(defn parse-message [msg]
+  (re-find #"^@(\w{6,})\s*喜欢\s*@(\w{6,})\s*(.*)" msg))
 
-(defn parsecontent [content]
-  (re-find #"^@(\w{6,})\s*喜欢\s*@(\w{6,})\s*(.*)" content))
+(defn parse-register [msg]
+  (re-find #"^@(\w{6,})\s*喜欢\s*(?????)" msg))
 
 (defn reply-text [from to content]
   (println "reply msg" from to content)
@@ -74,16 +143,16 @@
     (case msgtype
       "text" (let [content (utils/xml-find :Content xs)]
                (case content
-                 ("h" "H") (resp HELP)
-						     ("c" "C") (resp (checklove from))
+                 ("h" "H") (resp (if (registered? from) HELP REGISTER-HELP))
+						     ("c" "C") (resp (ta-like-you from))
 							   (let [ret (parsecontent content)]
 							     (if (nil? ret)
 							       (resp HELP)
-							       (let [[_ a b loveword] ret]
-							         (resp (love from a b loveword)))))))
+							       (let [[_ a b sweetwords] ret]
+							         (resp (love from a b sweetwords)))))))
       "event" (let [ev (utils/xml-find :Event xs)]
                 (case ev
-                  "subscribe" (resp HELP)
+                  "subscribe" (resp WELCOME-MSG)
                   "unsubscribe" (resp "unsubscribed!")
                   "CLICK" (resp "clicked")))
       (resp "目前不支持此消息类型"))))
@@ -92,7 +161,6 @@
 (defroutes all-routes
   (GET "/auth" [] auth)
   (POST "/auth" {body :body} (reply (slurp body)))
-  (GET "/infos" [] infos)
   (route/not-found "<p>Page not found.</p>"))
 
 (defn init-db []
